87363e28b3d0146a56b1582c81faebe641ce6b97
Math <philipponmathis@gmail.com>
2024-07-11 12:42:02 +0200

    Creature FreeRoaming

---
+++ Assets/Scripts/AIBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AIBehavior : MonoBehaviour
+{
+    private GameHandler _gameHandler;
+
+    private Vector3 _target;
+    
+    private NavMeshAgent _agent;
+    
+    public void Generate(GameHandler ga)
+    {
+        _gameHandler = ga;
+        _target = this.gameObject.transform.position;
+        _agent = this.gameObject.AddComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        //Check if the distance to target is less than the threshold
+        if (Vector3.Distance(this.gameObject.transform.position, _target) < _gameHandler.creaturesRoamingThreshold)
+        {
+            //Get random point on navmesh
+            _target = _gameHandler.RandomNavmeshLocation(_gameHandler.creaturesRoamingRange);
+        }
+        
+        //Move towards target using Navmesh
+        _agent.SetDestination(_target);
+    }
+}
--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -17,12 +17,15 @@ public class GameHandler : MonoBehaviour
     public GameObject playerPrefab;
     public Vector3 playerStart = new Vector3(0, 0.5f, 0);
     public int toGenerate = 2;
+    public float creaturesRoamingThreshold = 20.0f;
+    public float creaturesRoamingRange = 500.0f;
+    public float creatureSpreadRange = 5000.0f;
 
     private GameObject _player;    
     private GameObject _goal;
     private GameObject _creatures;
     
-    // Start is called before the first frame update
+    // Start is called before the first frame update&
     void Start()
     {
         generator.Generate();
@@ -30,13 +33,16 @@ public class GameHandler : MonoBehaviour
         navMeshSurface.BuildNavMesh();
 
         _creatures = blenderCLIHandler.generate(seed, toGenerate);
-
         for (int i = 0; i < _creatures.transform.childCount; i++)
         {
             GameObject obj = _creatures.transform.GetChild(i).gameObject;
             //Get random point on navmesh
-            obj.transform.position = RandomNavmeshLocation(50);
+            obj.transform.position = RandomNavmeshLocation(creatureSpreadRange);
             obj.transform.localScale = new Vector3(0.1f, 0.25f, 0.1f);
+            
+            //Add AI behavior script
+            AIBehavior aiBehavior = obj.AddComponent<AIBehavior>();
+            aiBehavior.Generate(this);
         }
         
         _goal = generator.GetWinArea();
@@ -44,7 +50,7 @@ public class GameHandler : MonoBehaviour
         _player = Instantiate(playerPrefab, playerStart, Quaternion.identity);
     }
     
-    private Vector3 RandomNavmeshLocation(float radius) 
+    public Vector3 RandomNavmeshLocation(float radius) 
     {
         Vector3 randomDirection = Random.insideUnitSphere * radius;
         randomDirection += transform.position;
